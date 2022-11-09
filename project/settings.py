ca952b5fa2cdc86cd7869d5872d038b74087d01a
Nithiesh Mohan <nithiesh164@gmail.com>
2022-11-10 00:46:14 +0530

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -140,5 +140,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'nithiesh164@gmail.com'
-EMAIL_HOST_PASSWORD = 'bowkmaqyeexzhmvi'
+EMAIL_HOST_USER = 'blogpostminorproject@gmail.com'
+EMAIL_HOST_PASSWORD = 
