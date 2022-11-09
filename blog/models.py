eee8407bccd6e3461a6703bdf6c1e5464b93586d
Nithiesh Mohan <nithiesh164@gmail.com>
2022-11-09 23:14:54 +0530

    Added CRUD Operations.

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 # For users and Posts we import from the model Users. Now the relationship
 # between Users and Posts are 1:M (cause a single user can have many different posts but a post can have
@@ -13,5 +14,8 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('post-detail', kwargs={'pk':self.pk})
 
 
--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,64 @@
 from django.shortcuts import render
 from .models import Post
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 def home(request):
     context = {
-        'posts' : Post.objects.all()
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'  # <app>/<model>_<viewtype>.html #Since we have a template already, we will be using that instead of blog/Post_list.html.
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+    #The model variable says where to create new posts or in which model to create new posts, In this case we want 
+     #to create new posts in the existing model posts.
+ 
+
+class PostDetailView(DetailView):
+    model = Post
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin,UpdateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+        
+
+class PostDeleteView(LoginRequiredMixin,UserPassesTestMixin,DeleteView):
+    model = Post
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+    success_url = '/'
+    
+
+
 def about(request):
     return render(request, 'blog/about.html', {'title':'About'})
+
+
