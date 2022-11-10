9a6f74180c51dac52f87749d8069c5b88eeca231
Nithiesh Mohan <nithiesh164@gmail.com>
2022-11-10 13:02:24 +0530

    Final Steps

--- project/settings.py
+++ project/settings.py
@@ -21,10 +21,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^@q(ogwn8=42kp@aa)_l8ed-plc2q=l@y87m!i3+lhz^&z#)yh'
+#SECRET_KEY = 'django-insecure-^@q(ogwn8=42kp@aa)_l8ed-plc2q=l@y87m!i3+lhz^&z#)yh'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
+
 
 ALLOWED_HOSTS = []
 
@@ -141,4 +145,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'blogpostminorproject@gmail.com'
-EMAIL_HOST_PASSWORD = 
+EMAIL_HOST_PASSWORD = ''
