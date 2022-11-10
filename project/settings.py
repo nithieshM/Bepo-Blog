acb1a95e2bb3158c4f1a9082bf5caf7367c57c27
Nithiesh Mohan <nithiesh164@gmail.com>
2022-11-10 13:31:38 +0530

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 #SECRET_KEY = 'django-insecure-^@q(ogwn8=42kp@aa)_l8ed-plc2q=l@y87m!i3+lhz^&z#)yh'
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-^@q(ogwn8=42kp@aa)_l8ed-plc2q=l@y87m!i3+lhz^&z#)yh')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -122,8 +122,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
-
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
